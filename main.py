04bc5714140e61c9bbc5a122bda32154d72e1071
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2023-05-16 01:43:54 +0300

    code with sessions was restructured into files, added new routes to main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Response
+from uuid import UUID
 
 from authentication.auth import app as AuthApp
+from system.sessions.create_session import create_session
+from system.sessions.read_session import whoami
+from system.sessions.delete_session import del_session
+from system.sessions.frontend import cookie
+from system.sessions.verifier import verifier
+from system.sessions.session_data import SessionData
 import db.db
 
 app = FastAPI(title="ChatterBox Backend App")
@@ -10,3 +17,15 @@ app.mount("/auth", AuthApp)
 @app.get("/id{uid}")
 def hello(uid: int):
     return uid
+
+@app.post("/create_session/{name}")
+async def create_session_route(name: str, response: Response):
+    return await create_session(name, response)
+
+@app.get("/whoami", dependencies=[Depends(cookie)])
+async def whoami_route(session_data: SessionData = Depends(verifier)):
+    return await whoami(session_data)
+
+@app.post("/delete_session")
+async def del_session_route(response: Response, session_id: UUID = Depends(cookie)):
+    return await del_session(response, session_id)
