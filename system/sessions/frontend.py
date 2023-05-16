1f2be2bfa6aac7854fef638d585b42b4938a2599
Mariam-Guseinova <mariam.guseinova.itmo@gmail.com>
2023-05-16 22:19:55 +0300

    Secret key was moved to .env file and added in config file

--- system/sessions/frontend.py
+++ system/sessions/frontend.py
@@ -1,4 +1,5 @@
 from fastapi_sessions.frontends.implementations import SessionCookie, CookieParameters
+from ...config import SECRET_KEY
 
 cookie_params = CookieParameters()
 
@@ -7,6 +8,6 @@ cookie = SessionCookie(
     cookie_name="cookie",
     identifier="general_verifier",
     auto_error=True,
-    secret_key="b410ffcb40fe3f9cd37ab32dad0af9be2b30c2f379fa60e62e5c77b821a22a44",
+    secret_key=SECRET_KEY,
     cookie_params=cookie_params,
 )
