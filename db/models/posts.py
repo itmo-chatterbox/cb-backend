e0a3072fef7b97f27b6db15181a72b7918ca8f17
Alexandr <75446587+be1ov@users.noreply.github.com>
2023-06-14 12:29:31 +0300

    Revert "Wall"

--- db/models/posts.py
+++ db/models/posts.py
@@ -8,5 +8,4 @@ from db.models.users import User
 class Post(BaseModel):
     user_id = ForeignKeyField(User, related_name="Posts")
     date_of_publication = DateField(null=False)
-    comment = CharField(null=True)
-    title = CharField(null=True)
+    comment = CharField(null=False)
